60ce37c04aaca9c3a9a174527d5a91f8738c4d5d
Dr. Steve <Doc-Steve@users.noreply.github.com>
2026-01-14 17:16:48 +0100

    pkgs-by-name

--- modules/nix/tools/pkgs-by-name [G]/pkgs-by-name.nix
+++ modules/nix/tools/pkgs-by-name [G]/pkgs-by-name.nix
@@ -4,29 +4,6 @@
   ...
 }:
 {
-  flake-file.inputs = {
-    pkgs-by-name-for-flake-parts.url = "github:drupol/pkgs-by-name-for-flake-parts";
-    packages = {
-      url = "path:./packages";
-      flake = false;
-    };
-  };
-
-  imports = [
-    inputs.pkgs-by-name-for-flake-parts.flakeModule
-  ];
-
-  perSystem =
-    { system, ... }:
-    {
-      pkgsDirectory = inputs.packages;
-    };
-
-  flake = {
-    overlays.default = _final: prev: {
-      local = withSystem prev.stdenv.hostPlatform.system ({ config, ... }: config.packages);
-    };
-  };
 
   flake.modules.generic.pkgs-by-name =
     { pkgs, ... }:
